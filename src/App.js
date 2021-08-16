42a49ab85f29cf362f3890edea46e861705e8e12
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-08-16 13:56:15 +0200

    final push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import {ChatEngine} from 'react-chat-engine';
-
+import LoginForm from './components/LoginForm'
 import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () =>{
+    if(!localStorage.getItem('username')) return <LoginForm/>
     return(
         <ChatEngine
             height="100vh"
             projectID="be4dccec-e4aa-47bd-92e5-ae706ba680c8"
-            userName="Bart"
-            userSecret="123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps}/>}
         />
     );
